e88ea836389fe8d3a0fe5152563e305467e74d7e
mdgarrison <mdgarrison@gmail.com>
2016-12-11 12:51:40 -0600

    Adding connection logic

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -9,8 +9,22 @@ class MyApp(QMainWindow):
         super(MyApp, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        # Here's an example of connecting a widget to Python code; in this case,
+        # the clicked status of a QButton is connected to a Python method
         self.ui.pushButton1.clicked.connect(self.HandlePushButton1Clicked)
         self.ui.pushButton2.clicked.connect(self.HandlePushButton2Clicked)
+
+        # Here's an example of connecting 2 widgets without any intervening code;
+        # in this case, the value of a QLcdNumber widget is connected to the
+        # value of a QDial
+        self.ui.dial1.valueChanged.connect(self.ui.lcdNumber1.display)
+        self.ui.horizontalSlider1.valueChanged.connect(self.ui.lcdNumber2.display)
+
+        # Here's another example of connecting 2 widgets outside of the code; in
+        # case, a little more unusual connection between the values of the two
+        # sliders
+        self.ui.horizontalSlider1.valueChanged.connect(self.ui.verticalSlider1.setValue)
 
     def HandlePushButton1Clicked(self):
         self.ui.textEdit1.setText("Hello")
