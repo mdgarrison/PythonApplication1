416f2904e8aa82e9e1ccb6320be59bf61e9405c5
mdgarrison <mdgarrison@gmail.com>
2016-12-11 21:01:15 -0600

    More slot-and-signal shennanigans

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
+from PyQt5.QtCore import pyqtSignal
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType("MainWindow.ui")
 
@@ -20,20 +21,35 @@ class MyApp(QMainWindow):
         # value of a QDial
         self.ui.dial1.valueChanged.connect(self.ui.lcdNumber1.display)
         self.ui.horizontalSlider1.valueChanged.connect(self.ui.lcdNumber2.display)
+        self.ui.dial1.valueChanged.connect(self.ui.progressBar1.setValue)
 
         # Here's another example of connecting 2 widgets outside of the code; in
         # case, a little more unusual connection between the values of the two
         # sliders
         self.ui.horizontalSlider1.valueChanged.connect(self.ui.verticalSlider1.setValue)
 
+    # This section of code shows an example of how to create a signal, how to connect it,
+    # and how to emit it
+
+    trigger = pyqtSignal()
+
+    def connect_trigger(self):
+        self.trigger.connect(self.handle_trigger)
+
+    def handle_trigger(self):
+        print("trigger signal received")
+
     def HandlePushButton1Clicked(self):
         self.ui.textEdit1.setText("Hello")
+        self.trigger.emit()
 
     def HandlePushButton2Clicked(self):
         self.ui.textEdit1.setText("World")
+        self.trigger.emit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
+    window.connect_trigger()
     window.show()
     sys.exit(app.exec_())
