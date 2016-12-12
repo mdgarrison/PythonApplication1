f859088ec51d778a14065c19aa4a606ac72411fe
MD Garrison <mdgarris@rockwellcollins.com>
2016-12-12 09:05:37 -0600

    More comments

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,18 +3,16 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSignal
 
+# This is pretty standard stuff for being able to load a .ui file
 Ui_MainWindow, QtBaseClass = uic.loadUiType("MainWindow.ui")
 
 class MyApp(QMainWindow):
     def __init__(self):
         super(MyApp, self).__init__()
+
+        # Here's more of the plumbing required to load a .ui file
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
-        # Here's an example of connecting a widget to Python code; in this case,
-        # the clicked status of a QButton is connected to a Python method
-        self.ui.pushButton1.clicked.connect(self.HandlePushButton1Clicked)
-        self.ui.pushButton2.clicked.connect(self.HandlePushButton2Clicked)
 
         # Here's an example of connecting 2 widgets without any intervening code;
         # in this case, the value of a QLcdNumber widget is connected to the
@@ -22,9 +20,18 @@ class MyApp(QMainWindow):
         self.ui.dial1.valueChanged.connect(self.ui.lcdNumber1.display)
         self.ui.horizontalSlider1.valueChanged.connect(self.ui.lcdNumber2.display)
         self.ui.dial1.valueChanged.connect(self.ui.progressBar1.setValue)
+        # Get in the habit of reading it like this:
+        # CONNECT FROM(dial1.valueChanged) TO(progressBar1.setValue)
+        #     The object we call connect() on is the sender
+        #     The object we pass in as the parameter is the receiver
+
+        # Here's an example of connecting a widget to Python code; in this case,
+        # the clicked status of a QButton is connected to a Python method
+        self.ui.pushButton1.clicked.connect(self.HandlePushButton1Clicked)
+        self.ui.pushButton2.clicked.connect(self.HandlePushButton2Clicked)
 
         # Here's another example of connecting 2 widgets outside of the code; in
-        # case, a little more unusual connection between the values of the two
+        # this case, a little more unusual connection between the values of the two
         # sliders
         self.ui.horizontalSlider1.valueChanged.connect(self.ui.verticalSlider1.setValue)
 
@@ -47,9 +54,18 @@ class MyApp(QMainWindow):
         self.ui.textEdit1.setText("World")
         self.trigger.emit()
 
+    # Here is an example of a function defined using type annotations (PEP 484)
+    def greeting(name: str) -> str:
+        return 'Hello ' + name
+
 if __name__ == "__main__":
+    # More generic plumbing -- this is how our app starts up
     app = QApplication(sys.argv)
     window = MyApp()
+
+    # A non-standard addition -- here is where I connect the trigger
     window.connect_trigger()
+
+    # Back to generic stuff
     window.show()
     sys.exit(app.exec_())
